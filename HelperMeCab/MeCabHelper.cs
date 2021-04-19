949ad805ccebf5e773333f9965e50f49a1a78610
ZUIcat <35529568+ZUIcat@users.noreply.github.com>
2021-04-20 00:12:26 +0800

    Add HelperMeCab

---
+++ HelperMeCab/MeCabHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NMeCab;
+using NMeCab.Core;
+using NMeCab.Specialized;
+
+namespace HelperMeCab {
+    public class MeCabHelper {
+        private static MeCabIpaDicTagger ipaDicTagger = null;
+        private static MeCabUniDic22Tagger uniDicTagger = null;
+
+        private MeCabHelper() { }
+
+        public static MeCabIpaDicTagger GetIpaDicTagger(string dicDir, string[] userDirDics = null) {
+            if (uniDicTagger != null) { uniDicTagger.Dispose(); }
+            if (ipaDicTagger == null) { ipaDicTagger = MeCabIpaDicTagger.Create(dicDir, userDirDics); }
+            return ipaDicTagger;
+        }
+
+        public static MeCabUniDic22Tagger GetUniDicTagger(string dicDir, string[] userDirDics = null) {
+            if (ipaDicTagger != null) { ipaDicTagger.Dispose(); }
+            if (uniDicTagger == null) { uniDicTagger = MeCabUniDic22Tagger.Create(dicDir, userDirDics); }
+            return uniDicTagger;
+        }
+    }
+}
