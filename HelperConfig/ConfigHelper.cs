7695a427778a08ae3cd1768727e21cc650d0e03a
ZUIcat <35529568+ZUIcat@users.noreply.github.com>
2021-04-20 21:05:12 +0800

    Add HelperConfig

---
+++ HelperConfig/ConfigHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace HelperConfig
+{
+    public class ConfigHelper
+    {
+    }
+}
